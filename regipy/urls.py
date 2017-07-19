1a249d94757e1a22b6a8f01aa74d6c083365f163
Chris Beldam <chrisgbeldam@gmail.com>
2017-07-19 18:01:41 +0100

    Login

--- regipy/urls.py
+++ regipy/urls.py
@@ -19,4 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('regipys.urls', namespace='regipys')),
+    url(r'^users/', include('users.urls', namespace='users')),
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth import logout
+
+def logout_view(request):
+    """Log the user out."""
+    logout(request)
+    return HttpResponseRedirect(reverse('users:login'))
